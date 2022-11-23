779cc7bb6d571aae9c11fac14a289cf85485dea8
HanYuan <max71239977@gmail.com>
2022-11-23 16:21:56 +0800

    Initial Commit

---
+++ UIBezierPath/UIBezierPathApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIBezierPathApp.swift
+//  UIBezierPath
+//
+//  Created by HanYuan on 2022/11/23.
+//
+
+import SwiftUI
+
+@main
+struct UIBezierPathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
